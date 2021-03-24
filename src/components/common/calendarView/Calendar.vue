0c0e290b7a4bc5b56b55696c1006ae88d03338e1
anhngan272@gmail.com <anhngan272@gmail.com>
2021-03-24 20:35:42 +0700

    add placeholder events to calendar

--- src/components/common/calendarView/Calendar.vue
+++ src/components/common/calendarView/Calendar.vue
@@ -2,9 +2,10 @@
   <div class="wrap">
     <a-alert
       :message="`You selected date: ${
-        selectedValue && selectedValue.format('YYYY-MM-DD')
+        selectedValue && selectedValue.format('DD-MM-YYYY')
       }`"
     />
+
     <div
       :style="{
         display: 'inline-block',
@@ -17,7 +18,19 @@
         :value="value"
         @select="onSelect"
         @panelChange="onPanelChange"
-      />
+      >
+        <ul slot="dateCellRender" slot-scope="value" class="events">
+          <li v-for="item in getListData(value)" :key="item.content">
+            <a-badge :color="item.color" :text="item.content" />
+          </li>
+        </ul>
+        <template slot="monthCellRender" slot-scope="value">
+          <div v-if="getMonthData(value)" class="notes-month">
+            <section>{{ getMonthData(value) }}</section>
+            <span>Backlog number</span>
+          </div>
+        </template>
+      </a-calendar>
     </div>
   </div>
 </template>
@@ -28,9 +41,15 @@ export default {
   name: "Calendar",
   data() {
     return {
-      value: moment("2017-01-25"),
-      selectedValue: moment("2017-01-25"),
-      value1: moment("2017-01-25"),
+      value: moment(new Date()),
+      selectedValue: moment(new Date()),
+      // value1: moment("2017-01-25"),
+      badgeColor: {
+        red: "#ff3e30",
+        blue: "#176bef",
+        yellow: "#f7b529",
+        green: "#179b52",
+      },
     };
   },
   methods: {
@@ -41,9 +60,63 @@ export default {
     onPanelChange(value) {
       this.value = value;
     },
+    getListData(value) {
+      let listData;
+      switch (value.date()) {
+        case 8:
+          listData = [
+            { color: this.badgeColor.red, content: "This is warning event." },
+            { color: this.badgeColor.green, content: "This is usual event." },
+          ];
+          break;
+        case 10:
+          listData = [
+            { color: this.badgeColor.blue, content: "This is warning event." },
+            { color: this.badgeColor.yellow, content: "This is usual event." },
+            { color: this.badgeColor.yellow, content: "This is error event." },
+          ];
+          break;
+        case 15:
+          listData = [
+            { color: this.badgeColor.blue, content: "This is warning event" },
+            {
+              color: this.badgeColor.red,
+              content: "This is very long usual event。。....",
+            },
+          ];
+          break;
+        default:
+      }
+      return listData || [];
+    },
+
+    getMonthData(value) {
+      if (value.month() === 8) {
+        return 1394;
+      }
+    },
   },
 };
 </script>
 
-<style>
+<style scoped>
+.events {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.events .ant-badge-status {
+  overflow: hidden;
+  white-space: nowrap;
+  width: 100%;
+  text-overflow: ellipsis;
+  font-size: 12px;
+}
+.notes-month {
+  text-align: center;
+  font-size: 28px;
+}
+.notes-month section {
+  font-size: 28px;
+}
 </style>
