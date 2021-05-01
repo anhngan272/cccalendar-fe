b2d1a34082b80d7cd798d57095d648432d520bc8
huy.vo <huysgh94@gmail.com>
2021-05-02 02:22:45 +0700

    Setup for deployment

--- src/assets/config_example.js
+++ src/assets/config_example.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://cccalendar.pro:8000/api';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'https://api.cccalendar.online' : 'http://cccalendar.pro:8000/api';
 export const COOKIE_PATH = '/';
 export const DEFAULT_TITLE = 'CCCalendar';
